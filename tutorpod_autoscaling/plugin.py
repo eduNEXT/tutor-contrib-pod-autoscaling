f06299849bbfec7ce09a68c51f48885bae34d2b0
Jhony Avella <jhony.avella@edunext.co>
2023-02-21 09:48:18 -0500

    feat: first plugin approach supporting HPA

--- tutorpod_autoscaling/plugin.py
+++ tutorpod_autoscaling/plugin.py
@@ -10,38 +10,95 @@ from tutor import hooks
 
 from .__about__ import __version__
 
+################# Autoscaling
+# Here comes the default common setttings for the autoscaling. Some resources
+# should not request a specific resources, but rather use as much/as few as
+# possible. This is the case for the CPU and memory in the case of workers.
+# Hence workers has no "*_MEMORY_REQUEST" but only "*_MEMORY_LIMIT" settings.
+# The LMS and CMS (non-worker) memory limits are set to have a small amount of
+# overcommitment. This is to prevent the LMS and CMS from being slow for
+# learners and instructors, though workers may lag a bit behind with tasks.
+CMS_MEMORY_REQUEST_MB = 350
+CMS_MAX_REPLICAS = 4
+CMS_WORKER_MEMORY_REQUEST_MB = 750
+
+LMS_MEMORY_REQUEST_MB = 350
+LMS_MAX_REPLICAS = 4
+LMS_WORKER_MEMORY_REQUEST_MB = 750
+
 ########################################
 # CONFIGURATION
 ########################################
 
+config = {
+    # Add here your new settings
+    "defaults": {
+        "VERSION": __version__,
+
+        "CMS_MEMORY_REQUEST": f"{CMS_MEMORY_REQUEST_MB}Mi",
+        "LMS_MEMORY_REQUEST": f"{LMS_MEMORY_REQUEST_MB}Mi",
+
+        # Kubernetes autoscaling settings
+        "CMS_AUTOSCALING": True,
+        "CMS_AVG_CPU": 300,
+        "CMS_AVG_MEMORY": "",
+        "CMS_CPU_LIMIT": 1,
+        "CMS_CPU_REQUEST": 0.25,
+        "CMS_MAX_REPLICAS": CMS_MAX_REPLICAS,
+        "CMS_MEMORY_LIMIT": f"{CMS_MEMORY_REQUEST_MB * 4}Mi",
+        "CMS_MIN_REPLICAS": 1,
+
+        "CMS_WORKER_AUTOSCALING": True,
+        "CMS_WORKER_AVG_CPU": 400,
+        "CMS_WORKER_AVG_MEMORY": "", # Disable memory-based autoscaling
+        "CMS_WORKER_CPU_LIMIT": 1,
+        "CMS_WORKER_CPU_REQUEST": 0.175,
+        "CMS_WORKER_MAX_REPLICAS": CMS_MAX_REPLICAS * 1.5,
+        "CMS_WORKER_MEMORY_LIMIT": f"{CMS_WORKER_MEMORY_REQUEST_MB * 4}Mi",
+        "CMS_WORKER_MEMORY_REQUEST": f"{CMS_WORKER_MEMORY_REQUEST_MB}Mi",
+        "CMS_WORKER_MIN_REPLICAS": 1,
+
+        "LMS_AUTOSCALING": True,
+        "LMS_AVG_CPU": 300,
+        "LMS_AVG_MEMORY": "",
+        "LMS_CPU_LIMIT": 1,
+        "LMS_CPU_REQUEST": 0.25,
+        "LMS_MAX_REPLICAS": LMS_MAX_REPLICAS,
+        "LMS_MEMORY_LIMIT": f"{LMS_MEMORY_REQUEST_MB * 4}Mi",
+        "LMS_MIN_REPLICAS": 1,
+
+        "LMS_WORKER_AUTOSCALING": True,
+        "LMS_WORKER_AVG_CPU": 400,
+        "LMS_WORKER_AVG_MEMORY": "", # Disable memory-based autoscaling
+        "LMS_WORKER_CPU_LIMIT": 1,
+        "LMS_WORKER_CPU_REQUEST": 0.175,
+        "LMS_WORKER_MAX_REPLICAS": LMS_MAX_REPLICAS * 1.5,
+        "LMS_WORKER_MEMORY_LIMIT": f"{LMS_WORKER_MEMORY_REQUEST_MB * 4}Mi",
+        "LMS_WORKER_MEMORY_REQUEST": f"{LMS_WORKER_MEMORY_REQUEST_MB}Mi",
+        "LMS_WORKER_MIN_REPLICAS": 1,
+    },
+    # Add here settings that don't have a reasonable default for all users. For
+    # instance: passwords, secret keys, etc.
+    "unique": {},
+    # Danger zone! Add here values to override settings from Tutor core or other plugins.
+    "overrides": {},
+}
+
 hooks.Filters.CONFIG_DEFAULTS.add_items(
     [
-        # Add your new settings that have default values here.
-        # Each new setting is a pair: (setting_name, default_value).
-        # Prefix your setting names with 'POD_AUTOSCALING_'.
-        ("POD_AUTOSCALING_VERSION", __version__),
+        (f"POD_AUTOSCALING_{key}", value)
+        for key, value in config["defaults"].items()
     ]
 )
 
 hooks.Filters.CONFIG_UNIQUE.add_items(
     [
-        # Add settings that don't have a reasonable default for all users here.
-        # For instance: passwords, secret keys, etc.
-        # Each new setting is a pair: (setting_name, unique_generated_value).
-        # Prefix your setting names with 'POD_AUTOSCALING_'.
-        # For example:
-        ### ("POD_AUTOSCALING_SECRET_KEY", "{{ 24|random_string }}"),
+        (f"POD_AUTOSCALING_{key}", value)
+        for key, value in config["unique"].items()
     ]
 )
 
-hooks.Filters.CONFIG_OVERRIDES.add_items(
-    [
-        # Danger zone!
-        # Add values to override settings from Tutor core or other plugins here.
-        # Each override is a pair: (setting_name, new_value). For example:
-        ### ("PLATFORM_NAME", "My platform"),
-    ]
-)
+hooks.Filters.CONFIG_OVERRIDES.add_items(list(config["overrides"].items()))
 
 
 ########################################
@@ -145,6 +202,7 @@ hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
     [
         ("pod-autoscaling/build", "plugins"),
         ("pod-autoscaling/apps", "plugins"),
+        ("pod-autoscaling/k8s", "plugins"),
     ],
 )
 
